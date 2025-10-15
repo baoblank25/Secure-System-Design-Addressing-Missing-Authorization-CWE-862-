5f6c17aa28610c88f6ab8c78522c54ccbc75cb97
Brian Bao Hoang <102432842+baoblank25@users.noreply.github.com>
2025-10-15 02:26:40 -0700

    Update ModelFirstAdmin.java

--- Java CWE 862/src/guiFirstAdmin/ModelFirstAdmin.java
+++ Java CWE 862/src/guiFirstAdmin/ModelFirstAdmin.java
@@ -7,12 +7,6 @@ package guiFirstAdmin;
  * data manipulated by this MVC beyond accepting a username and password and then saving it in the
  * database.  When the code is enhanced for input validation, this model may be needed.</p>
  * 
- * <p> Copyright: Lynn Robert Carter © 2025 </p>
- * 
- * @author Lynn Robert Carter
- * 
- * @version 1.00		2025-08-15 Initial version
- *  
  */
 
 public class ModelFirstAdmin {
